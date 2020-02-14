b88797e3529289c242d37a4621aa4ce40ec6a9ff
Sreenadh TC <kesav.tc8@gmail.com>
2020-02-14 12:04:02 +0530

    Remove old line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
   protect_from_forgery with: :exception
-  helper_method :current_user
+
 
   def authenticate
     redirect_to :login unless user_signed_in?
