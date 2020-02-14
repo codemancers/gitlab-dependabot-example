3cad8bc5ec38e5309ba9dfc1001a5948dcd5be0a
Sreenadh TC <kesav.tc8@gmail.com>
2020-02-14 11:39:48 +0530

    Add helper functions for checking logged in status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
+  protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def authenticate
+    redirect_to :login unless user_signed_in?
+  end
 end
