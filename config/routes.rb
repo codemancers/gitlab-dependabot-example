5f47b37789f561e7937f237540a1cf2aab2aff14
Sreenadh TC <kesav.tc8@gmail.com>
2020-02-14 11:52:33 +0530

    Add routes for login, profile and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'login', to: redirect('/auth/github'), as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'me', to: 'me#show', as: 'me'
+
 end
